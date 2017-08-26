0fad5620285d209137751fe6e66192e05f769d95
Joe Rattazzi <joerattazzi@gmail.com>
2017-08-26 13:10:08 -0500

    Adding iTerm install steps

---
+++ osx_init_iterm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Intstall Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
