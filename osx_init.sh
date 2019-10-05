450fc0e23abc74332feef2c8d56b6204a3873bd5
Joe Rattazzi <joerattazzi@gmail.com>
2019-10-04 21:51:33 -0500

    Modernize

--- osx_init.sh
+++ osx_init.sh
@@ -9,13 +9,13 @@ cd ~/Downloads
 # Download iTerm2
 brew cask install iterm2
 
-# Install Atom Text Editor
-brew cask install atom
+# Install Visual Studio Code
+brew cask install visual-studio-code
 
 # Install Postman
 brew cask install postman
 
-# Install Google Chrome
+# Install Firefox
 brew cask install google-chrome
 
 #Install git
@@ -37,40 +37,26 @@ brew install zsh-autosuggestions
 # Install Maven
 mkdir ~/maven
 
+# 3.6.2
+curl -O http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.zip
+unzip apache-maven-3.6.2-bin.zip
+mv apache-maven-3.6.2 ~/maven/3.6.2
+rm apache-maven-3.6.2-bin.zip
+
 # 3.5.4
 curl -O http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip
 unzip apache-maven-3.5.4-bin.zip
 mv apache-maven-3.5.4 ~/maven/3.5.4
 rm apache-maven-3.5.4-bin.zip
 
-# 3.3.9
-curl -O https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip apache-maven-3.3.9-bin.zip
-mv apache-maven-3.3.9 ~/maven/3.3.9
-rm apache-maven-3.3.9-bin.zip
-
-# 3.2.5
-curl -O https://archive.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.zip
-unzip apache-maven-3.2.5-bin.zip
-mv apache-maven-3.2.5 ~/maven/3.2.5
-rm apache-maven-3.2.5-bin.zip
-
-# 3.0.5
-curl -O https://archive.apache.org/dist/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.zip
-unzip apache-maven-3.0.5-bin.zip
-mv apache-maven-3.0.5 ~/maven/3.0.5
-rm apache-maven-3.0.5-bin.zip
-
 chmod -R 755 ~/maven
 
 # Add maven configs to .zshrc
+echo "alias mvn362='ln -sf ~/maven/3.6.2/bin/mvn /usr/local/bin/mvn'" >> ~/.zshrc
 echo "alias mvn354='ln -sf ~/maven/3.5.4/bin/mvn /usr/local/bin/mvn'" >> ~/.zshrc
-echo "alias mvn339='ln -sf ~/maven/3.3.9/bin/mvn /usr/local/bin/mvn'" >> ~/.zshrc
-echo "alias mvn325='ln -sf ~/maven/3.2.5/bin/mvn /usr/local/bin/mvn'" >> ~/.zshrc
-echo "alias mvn305='ln -sf ~/maven/3.0.5/bin/mvn /usr/local/bin/mvn'" >> ~/.zshrc
 
 #Set Maven 3.5.4 to the default
-echo "mvn354" >> ~/.zshrc
+echo "mvn362" >> ~/.zshrc
 
 # Install rvm - Ruby
 brew install gpg
@@ -78,15 +64,19 @@ brew install gpg
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source /Users/jr017790/.rvm/scripts/rvm
-rvm install ruby-2.5.1
+rvm install ruby-2.6.2
+
+# Install Java 
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk8
+brew cask install adoptopenjdk12
 
 # Set up Java aliases
-echo "alias jdk6='export JAVA_HOME=\`/usr/libexec/java_home -v \"1.6\"\`'" >> ~/.zshrc
-echo "alias jdk7='export JAVA_HOME=\`/usr/libexec/java_home -v \"1.7\"\`'" >> ~/.zshrc
 echo "alias jdk8='export JAVA_HOME=\`/usr/libexec/java_home -v \"1.8\"\`'" >> ~/.zshrc
+echo "alias jdk12='export JAVA_HOME=\`/usr/libexec/java_home -v \"12\"\`'" >> ~/.zshrc
 
 #Set Java 8 to the default
-echo "jdk8" >> ~/.zshrc
+echo "jdk12" >> ~/.zshrc
 
 #Install Jarvis - personal assistant
 # source - https://github.com/sukeesh/Jarvis 
